5974de6833cf668f11befd8629913322f52becdb
vamoose <46032161+Randi2324@users.noreply.github.com>
2023-01-03 05:20:17 +0700

    Update app.py

--- app.py
+++ app.py
@@ -101,13 +101,16 @@ def detect_file(filename):
 
 @app.route('/detect_video/<filename>', methods=['GET', 'POST'])
 def detect_video(filename):
+    source0 = "/content/TA"
     source1 = url_for('get_source', filename=filename)
-    source = "/content/TA" + source1
-    print("source ", source)
-    yolo_detect.main_detect(weights, source, img_size, conf_thres, iou_thres)
-    detect_video_url = url_for('get_file', filename=filename)
-    print("detect_video_url ", detect_video_url)
-    return render_template('detect_video.html', detect_video_url=detect_video_url)
+    source_detection = source0 + source1
+    print("source_detection ", source_detection)
+    yolo_detect.main_detect(weights, source_detection, img_size, conf_thres, iou_thres)
+    source2 = url_for('get_file', filename=filename)
+    source_convert = source0 + source2
+    print("source_convert ", source_convert)
+    subprocess.run(["ffmpeg","-i",source_convert,"/content/TA/inference/output/output.mp4"])
+    return render_template('detect_video.html', detect_video_url="/inference/output/output.mp4")
 
 
 @app.route('/', methods=['GET', 'POST'])
