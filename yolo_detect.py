481389ac1fe4d2b4cee6fb493b79463b8b4819cd
vamoose <46032161+Randi2324@users.noreply.github.com>
2023-01-04 02:26:01 +0700

    Update yolo_detect.py

--- yolo_detect.py
+++ yolo_detect.py
@@ -121,7 +121,7 @@ def detect(save_img=False):
                         plot_one_box(xyxy, im0, label=label,
                                      color=colors[int(cls)], line_thickness=2)
 
-            Print time (inference + NMS)
+            # Print time (inference + NMS)
             print('%sDone. (%.3fs)' % (s, t2 - t1))
 
             # Stream results
